0a21f0b03c0961ab049fed48c75517b18acb1b45
kishore9392 <maddineninandakishore@gmail.com>
2023-09-17 03:43:15 +0000

    galleryApp

---
+++ src/components/ThumbnailItem/index.js
@@ -0,0 +1,27 @@
+import './index.css'
+
+const ThumbnailItem = props => {
+  const {imagesList, updateState, isValid} = props
+
+  const {thumbnailAltText, id, thumbnailUrl} = imagesList
+
+  const onTap = () => {
+    updateState(id)
+  }
+
+  const check = isValid ? 'img3' : ''
+  return (
+    <list className="list">
+      <button className="btn" type="button">
+        <img
+          src={thumbnailUrl}
+          className={`img2 ${check}`}
+          alt={thumbnailAltText}
+          onClick={onTap}
+        />
+      </button>
+    </list>
+  )
+}
+
+export default ThumbnailItem
